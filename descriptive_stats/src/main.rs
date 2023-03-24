62e16add0dcc55196440ce60706b83efc3fa2c0f
Brian <blperry5@gmail.com>
2023-03-23 22:51:37 -0700

    descriptive stats

---
+++ descriptive_stats/src/main.rs
@@ -0,0 +1,12 @@
+
+// Given a list of integers, use a vector and return the median 
+// (when sorted, the value in the middle position) and mode (the value that occurs most often; 
+// a hash map will be helpful here) of the list. 
+
+fn main() {
+   let mut numbers = [1, 4, 9, 5, 10, 70, 94, 52, 2, 44, 5];
+   numbers.sort();
+       println!("{:?}", numbers);
+   
+   
+}
