2bcd61656c846251df2c83c0939374fc1d820126
Brian <blperry5@gmail.com>
2023-05-30 02:57:30 -0700

    updated csv reader

--- csvtutor/src/main.rs
+++ csvtutor/src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    env,
+    error::Error,
+    ffi::OsString,
+    io,
+    process,
+};
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "PascalCase")]
+struct Record {
+    latitude: f64,
+    longitude: f64,
+    #[serde(deserialize_with = "csv::invalid_option")]
+    population: Option<u64>,
+    city: String,
+    state: String,
 }
+
+fn main() {
+    if let Err(err) = run() {
+        println!("{}", err);
+        process::exit(1);
+    }
+}
+
+fn run() -> Result<(), Box<dyn Error>> {
+    //let file_path = get_first_arg()?;
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    
+    for result in rdr.deserialize() {
+        let record: Record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
+fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
+    match env::args_os().nth(1) {
+        None => Err(From::from("expected 1 argument, but got none")),
+        Some(file_path) => Ok(file_path),
+    }
+}
+
