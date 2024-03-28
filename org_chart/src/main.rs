a7e89dfe2674e50126d3f5d8b17566d0e11dd3a7
Brian <blperry5@gmail.com>
2024-03-28 02:15:29 -0700

    brush up on rust

--- org_chart/src/main.rs
+++ org_chart/src/main.rs
@@ -1,67 +1,134 @@
-use std::io;
+//  Using a hash map and vectors, create a text interface to allow a user to add employee names to a department in a company.
+//  For example, “Add Sally to Engineering” or “Add Amir to Sales.”
+//  Then let the user retrieve a list of all people in a department or all people in the company by department, sorted alphabetically.
+//
+
+use std::io::{self, Write};
 use std::collections::HashMap;
-use std::num::ParseIntError;
+
 #[warn(unused_variables)]
 #[derive(Debug)]
-struct Adduser{
+enum Selection {
+    Add,
+    EmpDept,
+    EmpAll,
+    Quit,
 }
 fn main() {
     
-        let mut company_info: HashMap<String, Vec<String>> = HashMap::new();
+    let mut company_info: HashMap<String, Vec<String>> = Default::default();
 
     loop {
+        let input = promt_user();
 
-        let choice = promt_user();
-
-        println!("WTF = {}", choice);
-
-        let choice: u32 = choice.trim().parse().expect("FINE");
-        println!("JUST MAYBE.. {}", choice);
-    
-
-//        let mut fields = String::new();
-
-//        io::stdin()
-//            .read_line(&mut fields)
-//            .expect("couldn't read entries");
-
-//        let (dept, name)  = fields.trim().split_once(' ').unwrap();
-//        println!("Testing={:?} and Name={:?}", dept, name);
-        
-    
-       // company_info.entry(dept).or_insert(vec![name]);
-//        let new_name = company_info.entry(dept)
-//            .and_modify(|e| e.push(name.clone()))
-//            .or_insert_with(|| vec![name]);
-
-//        println!("Hashish test! = {:?}", company_info);
+        match input {
+            Selection::Add => add_employee(&mut company_info),
+            Selection::EmpDept => list_emp_dept(&company_info),
+            Selection::EmpAll => list_emp_all(&mut company_info),
+            Selection::Quit => quit(),
+        };
     } //loop
-
 } //main
 
-fn promt_user() -> String { 
-    println!("Enter action to take: ");
-    println!("1 ) Add employee to department list.");
-    println!("2 ) List employee(s) by department." );
-    println!("3 ) List all employee(s).");
-    println!("4 ) Quit.");
-
+fn promt_user() -> Selection { 
     let mut user_input = String::new();
-    io::stdin().read_line(&mut user_input);
+    loop {
+        println!("Enter action to take: ");
+        println!("1 ) Add employee to department list.");
+        println!("2 ) List employee(s) by department." );
+        println!("3 ) List all employee(s).");
+        println!("4 ) Quit.");
+        print!(":=> ");
+        let _ = io::stdout().flush();
 
-    return user_input
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Unable to read line");
+
+        if let Ok(parse_num) = user_input.trim().parse::<u32>() {
+            match parse_num {
+                1 => return Selection::Add,
+                2 => return Selection::EmpDept,
+                3 => return Selection::EmpAll,
+                4 => return Selection::Quit,
+                _ => { println!("Number must be between 1-4\n");
+                    user_input.clear();
+                    continue },
+            };
+        } else {
+            user_input.clear();
+            println!("That is not a number between 1-4\n");
+        };
+    }; //loop
 }   
 
-fn add_employee() {
-    println!("Enter the department name followed by employee name you would like to enter \\n");
-    let mut data_entry = String::new();
-    io::stdin()
-        .read_line(&mut data_entry)
-        .expect("Need to enter: A department name, space, employee name");
-    
-    let (dept, name) = data_entry.trim().split_once(' ').unwrap();
-    
-//    company_info.entry(String::from(dept)).or_insert_with();
+fn add_employee(db: &mut HashMap<String, Vec<String>>) {
+    let mut emp_entry = String::new();
 
+    loop {
+        println!("Enter the department name followed by employee name you would like to enter");
+        print!(":=> ");
+        let _ = io::stdout().flush();
 
+        io::stdin()
+            .read_line(&mut emp_entry)
+            .expect("Unable to read line");
+        
+       if let Some((dept, name)) = emp_entry.trim().split_once(' ') {
+            db.entry(String::from(dept))
+                .or_default()
+                .push(String::from(name));
+            println!("Entry added- Dept[{dept}] : Employee[{name}]\n");
+            break;
+        } else {
+            println!("You need to enter a Dept AND a name");
+            emp_entry.clear();
+            continue;
+        };
+    }; //loop
+}
+
+fn list_emp_dept(db: &HashMap<String, Vec<String>>) {
+    if db.is_empty() { 
+        println!("There are no Departments or Employees, add some :)\n");
+        return;
+    }
+
+    let mut dept_input = String::new();
+
+    loop {
+        println!("List of available departments:");
+
+        for k in db.keys() {
+            println!("Dept:[{k}]");
+        }
+
+        println!("Enter the department name to get list of employees:");
+        print!(":=> ");
+        let _ = io::stdout().flush();
+
+        io::stdin()
+            .read_line(&mut dept_input)
+            .expect("Expected a department name");
+        
+        let dept = dept_input.trim();
+
+        if let Some((_, names)) = db.get_key_value(dept) {
+            println!("{:?}", names);
+            break;
+        } else {
+            println!("That department doesn't exist");
+        }
+    }; // loop
+}
+
+fn list_emp_all(db: &mut HashMap<String, Vec<String>>) {
+    for (k,v) in db.iter_mut() {
+        v.sort();
+        println!("Dept:[{}]: Emp:{:?}", k, v);
+    };
+}
+
+fn quit() {
+    std::process::exit(0);
 }
